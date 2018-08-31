a49d11587b29fbbe28f2c9fe2ae56d47572e4caf
linycdreamer <u201611003@qq.com>
2018-08-31 13:36:45 +0800

    sogoupinyin 输入法

---
+++ sogou.sh
@@ -0,0 +1,7 @@
+#sudo add-apt-repository ppa:fcitx-team/nightly
+#sudo apt-get update
+sudo apt-get install fcitx
+sudo apt-get install fcitx-config-gtk
+sudo apt-get install fcitx-table-all
+sudo apt-get install im-switch
+sudo dpkg -i sogoupinyin_2.2.0.0108_amd64.deb
